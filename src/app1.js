bae76b27bc216145177ed3fb9dfe0a650b6dd8f7
jianfeng <gjf610@yeah.net>
2021-11-16 12:02:26 +0800

    重构为MVC

--- src/app1.js
+++ src/app1.js
@@ -1,36 +1,94 @@
 import $ from 'jquery'
 import './app1.css';
 
-const $button1 = $("#add1")
-const $button2 = $("#minus1")
-const $button3 = $("#mul2")
-const $button4 = $("#divide2")
-const $number = $("#number")
+const eventBus = $({})
 
-const num = localStorage.getItem('n')
-$number.text(num || 100)
 
-$button1.on("click", () => {
-    let n = parseInt($number.text());
-    n += 1;
-    localStorage.setItem('n', n);
-    $number.text(n);
-})
-$button2.on("click", () => {
-    let n = parseInt($number.text());
-    n -= 1;
-    localStorage.setItem('n', n);
-    $number.text(n);
-})
-$button3.on("click", () => {
-    let n = parseInt($number.text());
-    n *= 2;
-    localStorage.setItem('n', n);
-    $number.text(n);
-})
-$button4.on("click", () => {
-    let n = parseInt($number.text());
-    n /= 2;
-    localStorage.setItem('n', n);
-    $number.text(n);
-})
+const m = {
+    data: {
+        num: parseInt(localStorage.getItem('num')) || 100
+    },
+    create() { },
+    delete() { },
+    update(data) {
+        Object.assign(m.data, data)
+        eventBus.trigger('m:updated')
+        localStorage.setItem('num', m.data.num)
+    },
+    get() { }
+}
+
+
+const v = {
+    el: null,
+    html: `
+    <div>
+        <div class="output">
+            <span id="number">{{num}}</span>
+        </div>
+        <div class="actions">
+            <button id="add1">+1</button>
+            <button id="minus1">-1</button>
+            <button id="mul2">×2</button>
+            <button id="divide2">÷2</button>
+        </div>
+    </div>
+    `,
+    init(container) {
+        v.el = $(container);
+    },
+    render(num) {
+        if (v.el.children.length !== 0) v.el.empty()
+        $(v.html.replace('{{num}}', num))
+            .appendTo(v.el)
+
+    },
+}
+
+const c = {
+    init(container) {
+        // 初始化渲染
+        v.init(container)
+        v.render(m.data.num); // view = render(data)
+        c.autoBindEvents();
+        eventBus.on('m:updated', () => {
+            v.render(m.data.num)
+        })
+    },
+    events: {
+        'click #add1': 'add',
+        'click #minus1': 'minus',
+        'click #mul2': 'mul',
+        'click #divide2': 'divide',
+    },
+    add() {
+        m.update({ num: m.data.num + 1 })
+    },
+    minus() {
+        m.update({ num: m.data.num - 1 })
+
+    },
+    mul() {
+        m.update({ num: m.data.num * 2 })
+
+    },
+    divide() {
+        m.update({ num: m.data.num / 2 })
+    },
+    autoBindEvents() {
+        // 绑定事件
+        for (let key in c.events) {
+            const value = c[c.events[key]]
+            const spaceIndex = key.indexOf(' ')
+            const action = key.slice(0, spaceIndex)
+            const ids = key.slice(spaceIndex + 1)
+            v.el.on(action, ids, value)
+
+        }
+    }
+}
+
+export default c;
+
+
+
--- src/app2.js
+++ src/app2.js
@@ -1,19 +1,77 @@
 import $ from "jquery";
 import './app2.css';
-const $tabBar = $('#app2 .tab-bar');
-const $tabContent = $('#app2 .tab-content');
 
+const eventBus = $({})
 
-$tabBar.on('click', 'li', (e) => {
-    const $li = $(e.currentTarget);
-    $li.addClass("selected")
-        .siblings()
-        .removeClass('selected');
-    const index = $li.index()
-    console.log(index)
-    $tabContent.children()
-        .eq(index).addClass('active')
-        .siblings().removeClass('active')
-})
+const localKey = 'app2.index';
+const m = {
+    data: {
+        index: parseInt(localStorage.getItem(localKey)) || 0
 
-$tabBar.children().eq(0).trigger('click')
+    },
+    create() { },
+    delete() { },
+    update(data) {
+        Object.assign(m.data, data)
+        eventBus.trigger('m:updated')
+        localStorage.setItem('index', m.data.index)
+    },
+    get() { }
+}
+const v = {
+    el: null,
+    html: (index) => {
+        console.log('index', index)
+        return `
+        <div>
+            <ol class="tab-bar">
+                <li class="${index === 0 ? 'selected' : ''}" data-index="0"><span>1111</span></li>
+                <li class="${index === 1 ? 'selected' : ''}" data-index="1"><span>2222</span></li>
+            </ol>
+            <ol class="tab-content">
+                <li class="${index === 0 ? 'active' : ''}">内容1</li>
+                <li class="${index === 1 ? 'active' : ''}">内容2</li>
+            </ol>
+        </div>
+        `
+    },
+    init(container) {
+        v.el = $(container);
+    },
+    render(index) {
+        if (v.el.children.length !== 0) v.el.empty()
+        $(v.html(index)).appendTo(v.el)
+
+    },
+}
+const c = {
+    init(container) {
+        // 初始化渲染
+        v.init(container)
+        v.render(m.data.index); // view = render(data)
+        c.autoBindEvents();
+        eventBus.on('m:updated', () => {
+            v.render(m.data.index)
+        })
+    },
+    events: {
+        'click .tab-bar li': 'x',
+    },
+    x(e) {
+        const index = parseInt(e.currentTarget.dataset.index);
+        m.update({ index })
+    },
+    autoBindEvents() {
+        // 绑定事件
+        for (let key in c.events) {
+            const value = c[c.events[key]]
+            const spaceIndex = key.indexOf(' ')
+            const action = key.slice(0, spaceIndex)
+            const ids = key.slice(spaceIndex + 1)
+            v.el.on(action, ids, value)
+
+        }
+    }
+}
+
+export default c;
